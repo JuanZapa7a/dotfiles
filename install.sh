00d549fbac7da2867f6d35b5b6bf8ea470c273d7
juanzapa7a <juan.zapata@upct.es>
2021-12-16 17:53:23 +0100

    Added Picom

--- install.sh
+++ install.sh
@@ -12,12 +12,14 @@ ln -sf "$HOME/dotfiles/X11/.Xresources" "$HOME/.Xresources"
 
 # i3 #
 mkdir -p "$HOME/.config/i3"
-ln -sf "$HOME/dotfiles/i3/config" "$HOME/.config/i3/config" # -s symlink -f file that already exits
+ln -sf "$HOME/dotfiles/i3/config" "$HOME/.config/i3/config"
 
 mkdir -p "$HOME/.config/i3status"
-ln -sf "$HOME/dotfiles/i3status/config" "$HOME/.config/i3status/config" # -s symlink -f file that already exits
-ln -sf "$HOME/dotfiles/i3status/pyconfig" "$HOME/.config/i3status/pyconfig" # -s symlink -f file that already exits
+ln -sf "$HOME/dotfiles/i3status/config" "$HOME/.config/i3status/config"
+ln -sf "$HOME/dotfiles/i3status/pyconfig" "$HOME/.config/i3status/pyconfig" 
 
+mkdir -p "$HOME/.config/picom"
+ln -sf "$HOME/dotfiles/picom/picom.conf" "$HOME/.config/picom/picom.conf" 
 
 
 
